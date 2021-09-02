c088680a97b1b215507abca2df96f41f99ab6c23
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2021-09-02 16:47:58 +0800

    Integrated bookmark_api to mysite

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -30,12 +30,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "bookmark_api.apps.BookmarkApiConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
