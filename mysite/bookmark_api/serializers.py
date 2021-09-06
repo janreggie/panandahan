6c8867a6039e94ae8dd914958dc12b787fe33e47
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2021-09-06 20:44:08 +0800

    Updated serializers to work with models

--- mysite/bookmark_api/serializers.py
+++ mysite/bookmark_api/serializers.py
@@ -3,19 +3,30 @@ from rest_framework import serializers
 from .models import Bookmark, Category, Tag
 
 
-class BookmarkSerializer(serializers.HyperlinkedModelSerializer):
+class TagSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Bookmark
-        fields = ("link", "caption", "title")
+        model = Tag
+        fields = ["id", "name"]
 
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Category
-        fields = ["name"]
+        fields = ["id", "name"]
 
 
-class TagSerializer(serializers.HyperlinkedModelSerializer):
+class BookmarkSerializer(serializers.HyperlinkedModelSerializer):
+    categories = CategorySerializer(read_only=True, many=True)
+    tag = TagSerializer(read_only=True, many=True)
+
     class Meta:
-        model = Tag
-        fields = ["name"]
+        model = Bookmark
+        fields = ["id", "link", "caption", "title", "categories", "tag"]
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep["categories"] = CategorySerializer(
+            instance.categories.all(), many=True
+        ).data
+        rep["tags"] = TagSerializer(instance.tags.all(), many=True).data
+        return rep
